448b8d922cf3c873c32100a394b16163f56f7d2d
GregTCLTK <gregtcltk@gmail.com>
2020-01-10 20:20:22 +0100

    A fix

--- src/main/java/com/bbn/XZerkrypter/listener/ReadyListener.java
+++ src/main/java/com/bbn/XZerkrypter/listener/ReadyListener.java
@@ -1,6 +1,9 @@
 package com.bbn.XZerkrypter.listener;
 
+import com.bbn.XZerkrypter.XZerkrypter;
 import com.bbn.XZerkrypter.core.GameAnimator;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
@@ -8,6 +11,13 @@ public class ReadyListener extends ListenerAdapter {
 
     @Override
     public void onReady(ReadyEvent event) {
+        /*
+        for (Guild g : event.getJDA().getGuilds()) {
+            XZerkrypter.rethink.insertGuild(g.getId());
+        }
+        for (User u : event.getJDA().getUsers()) {
+            XZerkrypter.rethink.insertUser(u.getId());
+        } */
         System.out.println("Bot started!");
         new GameAnimator(event.getJDA()).start();
     }

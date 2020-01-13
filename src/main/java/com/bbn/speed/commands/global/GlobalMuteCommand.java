7773c5fe6d0b4afe7fa191bfd9195e305d93a995
GregTCLTK <gregtcltk@gmail.com>
2020-01-13 15:30:21 +0100

    GlobalMute improvement´s

--- src/main/java/com/bbn/speed/commands/global/GlobalMuteCommand.java
+++ src/main/java/com/bbn/speed/commands/global/GlobalMuteCommand.java
@@ -18,30 +18,56 @@ public class GlobalMuteCommand implements Command {
     @Override
     public void action(String[] args, MessageReceivedEvent event) {
         if (Speed.rethink.isTeam(event.getAuthor().getId())) {
-            if (event.getMessage().getMentionedUsers().size() > 0) {
-                for (User u : event.getMessage().getMentionedUsers()) {
+            if (event.getMessage().getMentionedUsers().size() == 1) {
+                if (Speed.rethink.isMuted(event.getMessage().getMentionedUsers().get(0).getId())) {
+                    Speed.rethink.setMuted(event.getMessage().getMentionedUsers().get(0).getId(), false);
+                    event.getTextChannel().sendMessage(new EmbedBuilder()
+                            .setTitle("Erfolgreich ausgeführt")
+                            .setDescription("Ich habe erfolgreich " + event.getMessage().getMentionedUsers().get(0).getAsTag() + " entmuted.")
+                            .setColor(Color.GREEN)
+                            .setTimestamp(Instant.now())
+                            .build()).queue();
+                } else {
+                    Speed.rethink.setMuted(event.getMessage().getMentionedUsers().get(0).getId(), true);
+                    event.getTextChannel().sendMessage(new EmbedBuilder()
+                            .setTitle("Erfolgreich ausgeführt")
+                            .setDescription("Ich habe erfolgreich " + event.getMessage().getMentionedUsers().get(0).getAsTag() + " gemuted.")
+                            .setColor(Color.GREEN)
+                            .setTimestamp(Instant.now())
+                            .build()).queue();
+                }
+            } else if (args[0].length() == 18) {
+                try {
+                    User u = event.getJDA().getUserById(args[0]);
                     if (Speed.rethink.isMuted(u.getId())) {
                         Speed.rethink.setMuted(u.getId(), false);
+                        event.getTextChannel().sendMessage(new EmbedBuilder()
+                                .setTitle("Erfolgreich ausgeführt")
+                                .setDescription("Ich habe erfolgreich " + u.getAsTag() + " entmuted.")
+                                .setColor(Color.GREEN)
+                                .setTimestamp(Instant.now())
+                                .build()).queue();
                     } else {
                         Speed.rethink.setMuted(u.getId(), true);
+                        event.getTextChannel().sendMessage(new EmbedBuilder()
+                                .setTitle("Erfolgreich ausgeführt")
+                                .setDescription("Ich habe erfolgreich " + u.getAsTag() + " gemuted.")
+                                .setColor(Color.GREEN)
+                                .setTimestamp(Instant.now())
+                                .build()).queue();
                     }
-                }
-                event.getTextChannel().sendMessage(new EmbedBuilder()
-                        .setTitle("Erfolgreich ausgeführt")
-                        .setDescription("Ich habe erfolgreich den Mute Status von " + event.getMessage().getMentionedUsers().size() + " User gändert.")
-                        .setColor(Color.GREEN)
-                        .setTimestamp(Instant.now())
-                        .build()).queue();
-            } else if (args[0].length() == 18) {
-                if (Speed.rethink.isMuted(args[0])) {
-                    Speed.rethink.setMuted(args[0], false);
-                } else {
-                    Speed.rethink.setMuted(args[0], true);
+                } catch (Exception e) {
+                    event.getTextChannel().sendMessage(new EmbedBuilder()
+                            .setTitle("Nicht möglich")
+                            .setDescription("Der angegebene Benutzer war noch nie auf einem Server auf dem ich auch war.")
+                            .setColor(Color.RED)
+                            .setTimestamp(Instant.now())
+                            .build()).queue();
                 }
             } else {
                 event.getTextChannel().sendMessage(new EmbedBuilder()
                         .setTitle("Falsche Nutzung")
-                        .setDescription("Du musst mindestens einen User angeben.")
+                        .setDescription("Du musst genau einen User angeben.")
                         .setColor(Color.RED)
                         .setTimestamp(Instant.now())
                         .build()).queue();

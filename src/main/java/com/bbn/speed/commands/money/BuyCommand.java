b38fecaabf832d2f40f6322037b24939abe40847
GregTCLTK <gregtcltk@gmail.com>
2020-01-16 16:31:15 +0100

    Introduce buy command

---
+++ src/main/java/com/bbn/speed/commands/money/BuyCommand.java
@@ -0,0 +1,16 @@
+package com.bbn.speed.commands.money;
+
+/*
+ * @Author Skidder / GregTCLTK
+ */
+
+import com.bbn.speed.commands.Command;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+public class BuyCommand implements Command {
+    
+    @Override
+    public void action(String[] args, MessageReceivedEvent event) {
+
+    }
+}

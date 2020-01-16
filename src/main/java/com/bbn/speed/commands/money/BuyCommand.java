0ecd68648775b8e3ebc31c3a665cfbfbc8c0d470
GregTCLTK <gregtcltk@gmail.com>
2020-01-16 16:58:57 +0100

    Finish buy command

--- src/main/java/com/bbn/speed/commands/money/BuyCommand.java
+++ src/main/java/com/bbn/speed/commands/money/BuyCommand.java
@@ -4,6 +4,7 @@ package com.bbn.speed.commands.money;
  * @Author Skidder / GregTCLTK
  */
 
+import com.bbn.speed.Speed;
 import com.bbn.speed.commands.Command;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
@@ -16,7 +17,41 @@ public class BuyCommand implements Command {
     @Override
     public void action(String[] args, MessageReceivedEvent event) {
         if (args.length > 0) {
-
+            if (args[0].equals("premium")) {
+                if (Speed.rethink.getMoney(event.getAuthor().getId()) > 8000 || Speed.rethink.getMoney(event.getAuthor().getId()) == 8000) {
+                    try {
+                        event.getGuild().addRoleToMember(event.getMember(), event.getJDA().getGuildById("662472489317695490").getRoleById("664367891839189015")).reason("Premium gekauft").queue();
+                        Speed.rethink.setMoney(event.getAuthor().getId(), Speed.rethink.getMoney(event.getAuthor().getId()) - 8000);
+                        event.getTextChannel().sendMessage(new EmbedBuilder()
+                                .setTitle("Erfolgreich gekauft")
+                                .setDescription("Du hast dir erfolgreich Premium gekauft. Dir wurden 8000 Bäume abgezogen.")
+                                .setColor(Color.GREEN)
+                                .setTimestamp(Instant.now())
+                                .build()).queue();
+                    } catch (Exception e) {
+                        event.getTextChannel().sendMessage(new EmbedBuilder()
+                                .setTitle("Nicht möglich")
+                                .setDescription("Du musst auf dem offiziellen Speed Server sein um die Premium kaufen zu können.")
+                                .setColor(Color.RED)
+                                .setTimestamp(Instant.now())
+                                .build()).queue();
+                    }
+                } else {
+                    event.getTextChannel().sendMessage(new EmbedBuilder()
+                            .setTitle("Keine Bäume")
+                            .setDescription("Du benötigst 8000 Bäume um dir diesen Gegenstand zu kaufen.")
+                            .setColor(Color.RED)
+                            .setTimestamp(Instant.now())
+                            .build()).queue();
+                }
+            } else {
+                event.getTextChannel().sendMessage(new EmbedBuilder()
+                        .setTitle("Nicht existent")
+                        .setDescription("Der angegebene Gegnstand existiert nicht im Shop. Eine Liste aller Gegenstände ist mit `?shop` einsehbar.")
+                        .setColor(Color.RED)
+                        .setTimestamp(Instant.now())
+                        .build()).queue();
+            }
         } else {
             event.getTextChannel().sendMessage(new EmbedBuilder()
                     .setTitle("Kein Gegenstand")

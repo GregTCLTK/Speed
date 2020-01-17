6a835168ea15e5ca91a6e36040e1d1a74ef934c8
GregTCLTK <gregtcltk@gmail.com>
2020-01-17 14:28:40 +0100

    Fix some old stuff

--- src/main/java/com/bbn/speed/listener/ReadyListener.java
+++ src/main/java/com/bbn/speed/listener/ReadyListener.java
@@ -1,6 +1,9 @@
 package com.bbn.speed.listener;
 
+import com.bbn.speed.Speed;
 import com.bbn.speed.core.GameAnimator;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
@@ -10,10 +13,10 @@ public class ReadyListener extends ListenerAdapter {
     public void onReady(ReadyEvent event) {
         /*
         for (Guild g : event.getJDA().getGuilds()) {
-            XZerkrypter.rethink.insertGuild(g.getId());
+            Speed.rethink.insertGuild(g.getId());
         }
         for (User u : event.getJDA().getUsers()) {
-            XZerkrypter.rethink.insertUser(u.getId());
+            Speed.rethink.insertUser(u.getId());
         } */
         System.out.println("Bot started!");
         new GameAnimator(event.getJDA()).start();

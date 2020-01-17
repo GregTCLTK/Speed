b678a3a3c99056c81a92faa133dc7dbc51eae100
GregTCLTK <gregtcltk@gmail.com>
2020-01-17 23:44:25 +0100

    Introduce Rob Command

---
+++ src/main/java/com/bbn/speed/commands/fun/RobCommand.java
@@ -0,0 +1,16 @@
+package com.bbn.speed.commands.fun;
+
+/*
+ * @Author Skidder / GregTCLTK
+ */
+
+import com.bbn.speed.commands.Command;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+public class RobCommand implements Command {
+
+    @Override
+    public void action(String[] args, MessageReceivedEvent event) {
+
+    }
+}

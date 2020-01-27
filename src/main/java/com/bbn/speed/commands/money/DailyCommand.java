a33402bf2c69690550d27717dade0436dd5d3b4d
GregTCLTK <gregtcltk@gmail.com>
2020-01-27 19:24:51 +0100

    Better code yk

--- src/main/java/com/bbn/speed/commands/money/DailyCommand.java
+++ src/main/java/com/bbn/speed/commands/money/DailyCommand.java
@@ -17,27 +17,24 @@ public class DailyCommand implements Command {
     @Override
     public void action(String[] args, MessageReceivedEvent event) {
         if (Speed.rethink.getDailyTime(event.getAuthor().getId()) == null) {
-            Speed.rethink.setDailyTime(Instant.now(), event.getAuthor().getId());
-            Speed.rethink.setMoney(event.getAuthor().getId(), Speed.rethink.getMoney(event.getAuthor().getId()) + 150);
-            event.getTextChannel().sendMessage(new EmbedBuilder()
-                    .setTitle("Erfolgreich abgeholt")
-                    .setDescription("Du hast erfolgreich deine tägliche Belohnung abgeholt.")
-                    .setColor(Color.GREEN)
-                    .setTimestamp(Instant.now())
-                    .build()).queue();
+            collect(event);
         } else if (Speed.rethink.getDailyTime(event.getAuthor().getId()).isBefore(Instant.now().minusSeconds(86400L))) {
-            Speed.rethink.setDailyTime(Instant.now(), event.getAuthor().getId());
-            Speed.rethink.setMoney(event.getAuthor().getId(), Speed.rethink.getMoney(event.getAuthor().getId()) + 150);
-            event.getTextChannel().sendMessage(new EmbedBuilder()
-                    .setTitle("Erfolgreich abgeholt")
-                    .setDescription("Du hast erfolgreich deine tägliche Belohnung abgeholt.")
-                    .setColor(Color.GREEN)
-                    .setTimestamp(Instant.now())
-                    .build()).queue();
+            collect(event);
         } else event.getTextChannel().sendMessage(new EmbedBuilder()
                 .setTitle("Nicht möglich")
                 .setDescription("Du kannst nur alle 24 Stunden deine tägliche Belohnung abholen.")
                 .setColor(Color.RED)
+                .setTimestamp(Instant.now())
+                .build()).queue();
+    }
+
+    private void collect(MessageReceivedEvent event) {
+        Speed.rethink.setDailyTime(Instant.now(), event.getAuthor().getId());
+        Speed.rethink.setMoney(event.getAuthor().getId(), Speed.rethink.getMoney(event.getAuthor().getId()) + 150);
+        event.getTextChannel().sendMessage(new EmbedBuilder()
+                .setTitle("Erfolgreich abgeholt")
+                .setDescription("Du hast erfolgreich deine tägliche Belohnung abgeholt.")
+                .setColor(Color.GREEN)
                 .setTimestamp(Instant.now())
                 .build()).queue();
     }
